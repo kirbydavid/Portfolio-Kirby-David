db843d2dad76a8f4b9aaf265e1961220a2fa8b54
kirby <dvid.kirby@gmail.com>
2025-02-21 16:50:17 +0800

    base

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
  export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+
+      backgroundImage: {
+        'texture': "url('/images/texture.jpg')",
+      },
+    },
   },
   plugins: [],
 }
